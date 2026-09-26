b28aaafe58d6812bf3b5990455c94f32c528a7af
GuillermoRDR <guillermoreneroque@gmail.com>
2026-09-26 00:29:16 -0600

    Cambiar nombre de carpeta router a routers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const rutas = require('./router');
+const rutas = require('./routers');
 require('./config/db');
 require('dotenv').config();
 
